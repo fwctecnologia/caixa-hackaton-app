4468eeeadec268ecf1369bed43abeafcdf63160b
Anderson Mendes <anderson2000ano@gmail.com>
2023-07-29 12:31:28 -0300

    fix: add categorizacao

--- lib/app/modules/contas_cartoes/contas_cartoes.dart
+++ lib/app/modules/contas_cartoes/contas_cartoes.dart
@@ -1,3 +1,4 @@
+import 'package:caixa_hackaton_app/app/modules/categorizacao/categorizacao_screen.dart';
 import 'package:caixa_hackaton_app/app/modules/conquistas/conquistas_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -102,64 +103,69 @@ class _ContasCartoesScreenState extends State<ContasCartoesScreen> {
 }
 
 Widget card(title, value, description) {
-  return Container(
-    width: MediaQuery.of(Get.context!).size.width,
-    padding: EdgeInsets.all(8).copyWith(left: 12, right: 12),
-    decoration: BoxDecoration(
-      color: Color(0xFF505050),
-      borderRadius: BorderRadius.circular(8),
-    ),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          children: [
-            Image.asset(
-              'assets/x.png',
-              width: 50,
-            ),
-            Container(width: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  style: GoogleFonts.robotoCondensed(
-                    fontSize: 16,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  'R\$ ${value}',
-                  style: GoogleFonts.robotoCondensed(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            )
-          ],
-        ),
-        Container(height: 5),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              description,
-              style: GoogleFonts.robotoCondensed(
-                fontSize: 18,
-                fontWeight: FontWeight.w600,
-                color: Colors.white,
+  return GestureDetector(
+    onTap: () {
+      Get.to(() => CategorizacaoScreen());
+    },
+    child: Container(
+      width: MediaQuery.of(Get.context!).size.width,
+      padding: EdgeInsets.all(8).copyWith(left: 12, right: 12),
+      decoration: BoxDecoration(
+        color: Color(0xFF505050),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/x.png',
+                width: 50,
               ),
-            ),
-            Image.asset(
-              'assets/chevron_right_small.png',
-            ),
-          ],
-        )
-      ],
+              Container(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    style: GoogleFonts.robotoCondensed(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'R\$ ${value}',
+                    style: GoogleFonts.robotoCondensed(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+          Container(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                description,
+                style: GoogleFonts.robotoCondensed(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              Image.asset(
+                'assets/chevron_right_small.png',
+              ),
+            ],
+          )
+        ],
+      ),
     ),
   );
 }
